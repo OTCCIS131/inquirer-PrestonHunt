b840d09772d0675ca91961003b34ee221c5dc482
Preston Hunt <ph0859241@otc.edu>
2017-09-20 12:26:55 -0500

    minor changes

--- index.js
+++ index.js
@@ -2,38 +2,35 @@ const chalk = require('chalk')
 const inquirer = require('inquirer')
 
 
-//console.log("Welcome to Preston's Pizzaria")
 console.log(chalk.green('Welcome to'),chalk.white("Preston's"),chalk.red('Pizzaria'))
 console.log("Phone: 958-486-592")
 
 var questions = 
 [
     {
-        message: "What size would you like",
         name: 'pizzaSize',
+        message: "What size would you like",
         type: "list",
         choices: ['Small', 'Medium', 'Large']
     },
-
     {
+        name: 'toppings',
         message: 'What type of toppings would you like',
-        name: 'Toppings',
         type: 'checkbox',
         choices: [ 'pepperoni','sausage','canadian bacon','olives','onions','mushrooms']
     },
-
     {
-        message: 'How would you like your pizza cut',
         name: 'cut',
+        message: 'How would you like your pizza cut',
         type: 'list',
         choices: ['normal','square']
     }
 ]
 
-inquirer.prompt(questions).then(function ({pizzaSize,Toppings,cut})
+inquirer.prompt(questions).then(function ({pizzaSize,toppings,cut})
 {
     console.log('Order receipt')
     console.log("You wanted a " + pizzaSize + " pizza. \n"
-                + "With " + Toppings + " on it. \n"
+                + "With " + toppings + " on it. \n"
                 + "and cut in a " + cut + " fashion." )
 })
